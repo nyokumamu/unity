bfc8efcc4408ea87383c0b858ca91460da6602cd
hiraoka.yosuke <hiraoka.yosuke@dena.jp>
2014-04-15 20:41:27 +0900

    [master] jenga

---
+++ Jenga/Assets/script/TextDisplay.js
@@ -0,0 +1,18 @@
+﻿#pragma strict
+
+static var point : int = 0;
+static var isGameOver: int = 0;
+
+function OnGUI(){
+  GUI.Label(Rect(10,20,120,20), point + " point");
+}
+
+static function addPoint(_addPoint : int) {
+	point += _addPoint;
+}
+static function resetPoint() {
+	point = 0;
+}
+static function doGameOver(){
+	isGameOver = 1;
+}
